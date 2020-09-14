9ab72a43f3feeff9e592eadd40f4302360e1d849
Athena <atpa1875@colorado.edu>
2020-09-13 18:41:28 -0600

    first

---
+++ project1.java
@@ -0,0 +1,71 @@
+class Animal {
+  public static void wakeUp();
+  public static void makeNoise();
+  public static void eat();
+  public static void roam();
+  public static void sleep();
+}
+
+class Feline extends Animal {
+
+}
+
+class Lion extends Feline {
+
+}
+
+class Cat extends Feline {
+
+}
+
+class Tiger extends Feline {
+
+}
+
+class Canine extends Animal {
+
+}
+
+class Dog extends Canine {
+
+}
+
+class Wolf extends Canine {
+
+}
+
+class Pachyderm extends Animal {
+
+}
+
+class Hippo extends Pachyderm {
+
+}
+
+class Elephant extends Pachyderm {
+
+}
+
+class Rhino extends Pachyderm {
+
+}
+
+class Amphibian extends Animal {
+
+}
+
+class Frog extends Amphibian {
+
+}
+
+class Salamander extends Amphibian {
+
+}
+
+class Zookeeper {
+  public static void WakeAnimals();
+  public static void RollCall();
+  public static void FeedAnimals();
+  public static void ExerciseAnimals();
+  public static void SleepAnimals();
+}
